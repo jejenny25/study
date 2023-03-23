0c669d8ca73e5d36558f3b4d92f15195ebf86278
jejenny25 <jejenny25@gmail.com>
2023-03-23 13:08:39 +0900

    지우기

--- nicepay-client-for-react/src/api/index.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-const API_ENDPOINT = import.meta.env.VITE_APP_API_ENDPOINT
-const instance = axios.create({
-  baseURL: API_ENDPOINT,
-})
--- nicepay-client-for-react/src/vite-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  VITE_APP_API_ENDPOINT: string
-  VITE_APP_NICEPAY_CLIENTID: string
-  VITE_APP_NICEPAY_RETURNURL: string
-  VITE_APP_NICEPAY_SECRETKEY: string
-}
